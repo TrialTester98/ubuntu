c16f6582dc39908bdaef1aacf75195249f08707f
LunarEclipseCode <74882728+LunarEclipseCode@users.noreply.github.com>
2024-07-14 03:51:27 +0000

    Deploying to gh-pages from @ TrialTester98/ubuntu@90ca6588ad515d8c74a01354835e1fcaaccf6f78 🚀

---
+++ _next/static/Nk2iIy69m_WGl3y2Jqpq7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-7245d18b966de1f7.js","static/chunks/764-e4bb267b78f46c6d.js","static/chunks/pages/index-d1f3f19da92e4b27.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
